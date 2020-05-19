df7e1202ddd2194f844be66e38ea2c1aaac8766c
mauritscottyn <cottynmaurits@gmail.com>
2020-05-19 11:08:53 +0200

    configured path to haarcascades for both win32 and linux, removed eye detection

--- faceDetection/detection.py
+++ faceDetection/detection.py
@@ -1,23 +1,27 @@
 import cv2
+import sys
 
 """
-Finding the path to haarcascades on local system (windows only)
+Finding the path to haarcascades on local system
 """
-cv_file = cv2.__file__
-cv_file = str(cv_file)
-cv_file = result = cv_file.rsplit('\\', 1)[0]
-cv_file_face = cv_file + "\data\haarcascade_frontalface_alt.xml"
-cv_file_eyes = cv_file + "\data\haarcascade_eye_tree_eyeglasses.xml"
-print(cv_file_face)
+if sys.platform == 'win32':
+    print('windows')
+    cv_file = cv2.__file__ # Get the path to opencv executable
+    cv_file = str(cv_file)
+    cv_file = result = cv_file.rsplit('\\', 1)[0] # Leaves us at root of opencv directory
+    cv_file_face = cv_file + "\data\haarcascade_frontalface_alt.xml" 
+else:
+    cv_file = cv2.__file__ # Get the path to opencv executable
+    cv_file = str(cv_file)
+    cv_file = result = cv_file.rsplit('/', 1)[0] # Leaves us at root of opencv directory
+    cv_file_face = cv_file + "/data/haarcascade_frontalface_alt.xml" 
 
 
 """
-Creating face detection classifiers.
+Creating face detection classifier.
 """
 face_cascade = cv2.CascadeClassifier()
-eyes_cascade = cv2.CascadeClassifier()
 face_cascade.load(cv_file_face)
-eyes_cascade.load(cv_file_eyes)
 
 
 """
@@ -32,12 +36,6 @@ def detectAndDisplay(frame):
         center = (x + w//2, y + h//2)
         frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
         faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-        for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
     cv2.imshow('Capture - Face detection', frame)
 
 
