e356958fd673240ea8f83579221792adf4dfc6fe
mauritscottyn <cottynmaurits@gmail.com>
2020-05-19 17:03:00 +0200

    changed face detection to mtcnn

--- Models/faceDetection.py
+++ Models/faceDetection.py
@@ -2,14 +2,13 @@ import cv2
 import sys
 import threading
 import time
+from mtcnn import MTCNN
 
 class FaceDetection(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.path_to_haarcascade = self.get_haarcascade()
-        self.face_cascade = cv2.CascadeClassifier() # Face detection classifier
-        self.face_cascade.load(self.path_to_haarcascade)
-        self.cap = cv2.VideoCapture(0)    
+        self.detector = MTCNN()
+        self.cap = cv2.VideoCapture(0)    # Camera object
     
     def run(self):
         """
@@ -24,40 +23,17 @@ class FaceDetection(threading.Thread):
             if cv2.waitKey(10) == 27:
                 break
 
-    def get_haarcascade(self):
-        """
-        Finding the path to haarcascades on local system
-        """
-        if sys.platform == 'win32':
-            cv_file = cv2.__file__ # Get the path to opencv executable
-            cv_file = str(cv_file)
-            cv_file = cv_file.rsplit('\\', 1)[0] # Leaves us at root of opencv directory
-            cv_file = cv_file + "\data\haarcascade_frontalface_alt.xml"
-            return cv_file 
-        else:
-            cv_file = cv2.__file__ # Get the path to opencv executable
-            cv_file = str(cv_file)
-            cv_file = cv_file.rsplit('/', 1)[0] # Leaves us at root of opencv directory
-            cv_file = cv_file + "/data/haarcascade_frontalface_alt.xml"
-            return cv_file 
-
     def detectAndDisplay(self, frame):
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.equalizeHist(frame_gray)
-        #-- Detect faces
-        faces = self.face_cascade.detectMultiScale(frame_gray)
-        i = 0
-        for (x,y,w,h) in faces:
-            i += 1
-            cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0),thickness=4)
-            faceROI = frame_gray[y:y+h,x:x+w]
-            if sys.platform == 'win32':
-                file_name = f'data\detection\image{i}.jpg'
-            else:
-                file_name = f'data/detection/image{i}.jpg'
-                
-            faceROI = cv2.resize(faceROI,(160,160))    
-            cv2.imwrite(file_name, faceROI)
-        i = 0
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = self.detector.detect_faces(image)
+        if len(result) > 0:
+            box = result[0]['box']
+            if result[0]['confidence'] >= 0.9:
+                print('face found')
+                x1,y1,x2,y2 = box
+                faceROI = image[y1:y1+y2, x1:x1+x2]
+                cv2.imwrite('image.jpg', faceROI)
+                time.sleep(10)
+
         cv2.imshow('Capture - Face detection', frame)
 
