a08d8e3041e6b26bf72a0fe8bfb2153c6ca30b6c
mauritscottyn <cottynmaurits@gmail.com>
2020-05-20 15:15:58 +0200

    Updated face detection, now captures all faces in frame and converts them to tensors, frame rate can be adjusted

--- Models/faceDetection.py
+++ Models/faceDetection.py
@@ -2,38 +2,47 @@ import cv2
 import sys
 import threading
 import time
-from mtcnn import MTCNN
+from facenet_pytorch import MTCNN
 
 class FaceDetection(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.detector = MTCNN()
+        self.detector = MTCNN(keep_all=True) #detection classifier
         self.cap = cv2.VideoCapture(0)    # Camera object
+        self._detected_person = None
+
+    @property
+    def detected_person(self):
+        return self._detected_person
+    @detected_person.setter
+    def detected_person(self, value):
+        self._detected_person = value
     
     def run(self):
         """
-        Main thread that does the actual face detection.
+        Main thread: frame rate sets the amount of times the detect and convert function will be called per second.
         """
+        frame_rate = 2
+        prev = 0
         while True:
+            time_elapsed = time.time() - prev
             ret, frame = self.cap.read()
             if frame is None:
                 print('--(!) No captured frame -- Break!')
                 break
-            self.detectAndDisplay(frame)
+            if time_elapsed > 1./frame_rate:
+                prev = time.time()
+                self.detectAndConvert(frame)
             if cv2.waitKey(10) == 27:
                 break
 
-    def detectAndDisplay(self, frame):
+    def detectAndConvert(self, frame):
+        """
+        Function that handles the actual face detection. Detected faces are converted to tensors. 
+        Amount of detected faces can be found with len(self.detected_person)
+        """
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result = self.detector.detect_faces(image)
-        if len(result) > 0:
-            box = result[0]['box']
-            if result[0]['confidence'] >= 0.9:
-                print('face found')
-                x1,y1,x2,y2 = box
-                faceROI = image[y1:y1+y2, x1:x1+x2]
-                cv2.imwrite('image.jpg', faceROI)
-                time.sleep(10)
+        tensors = self.detector.forward(image)
+        self._detected_person = tensors
 
-        cv2.imshow('Capture - Face detection', frame)
 
