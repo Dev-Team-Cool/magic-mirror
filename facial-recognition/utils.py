300104c388c3cad4c8a14b6401ae13af719376f5
FlorianVD <vandaele.florian@hotmail.com>
2020-05-24 14:06:33 +0200

    added utility function to fetch the training images

---
+++ facial-recognition/utils.py
@@ -0,0 +1,15 @@
+import os
+
+
+def load_images(directory):
+    sub_dirs = os.scandir(directory)
+    images, labels = [], []
+    for sub_dir in sub_dirs:
+        if (sub_dir.is_dir()):
+            print('Directory:', sub_dir.name)
+            for img_file in os.scandir(sub_dir.path):
+                if img_file.is_file():
+                    images.append(img_file.path)
+                    labels.append(sub_dir)
+    
+    return images, labels
