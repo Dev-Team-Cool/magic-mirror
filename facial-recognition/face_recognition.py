5400267200238788e0a48cebd296ebc35a065587
FlorianVD <vandaele.florian@hotmail.com>
2020-05-24 15:00:38 +0200

    convert trainning examples to numpy array

--- facial-recognition/face_recognition.py
+++ facial-recognition/face_recognition.py
@@ -24,9 +24,7 @@ class FacialRecognition:
         return self.__model.predict(embedding)
     
     def __calculate_embedding(self, img_tensor: Tensor):
-        print(img_tensor.shape)
-        embedding = self.__resnet.forward(img_tensor.unsqueeze(0)).detach()
-        print(embedding.shape)
+        embedding = self.__resnet.forward(img_tensor.unsqueeze(0)).detach().numpy()
         return embedding
 
     def __generate_tensor(self, image):
@@ -35,11 +33,14 @@ class FacialRecognition:
 
         img_file = imread(image)
         return MTCNN().forward(img_file)
+
+    def train_classifier(self, training_data_path):
         from sklearn.neighbors import KNeighborsClassifier
         if self.__model: return
         
-        X_train, y_train = load_images('data/train')
-        X_train = [self.__calculate_embedding((self.__generate_tensor(image))) for image in X_train]
+        X_train, y_train = load_images(training_data_path)
+        X_train = np.array([self.__calculate_embedding((self.__generate_tensor(image))) for image in X_train])
+        X_train = X_train.reshape(-1, 512)
         knn_classifier = KNeighborsClassifier(n_neighbors=2)
         knn_classifier.fit(X_train, y_train)
         print('Model trained')
