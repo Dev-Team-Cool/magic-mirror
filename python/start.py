4c3075b2a8f259ed8041f962bcb7354e251cd2ad
FlorianVD <vandaele.florian@hotmail.com>
2020-06-05 16:44:14 +0200

    load the config before doing anything else

--- python/start.py
+++ python/start.py
@@ -1,4 +1,7 @@
 from facial_recognition.face_detection import FaceDetection
+from facial_recognition.config import Config
 
+# Load the config
+Config.load_config('/home/florian/Documents/school/magic-mirror/python/config.json')
 detection = FaceDetection()
 detection.start()
