60c78f6e3694bde4e38875976b5c5c59c46f9b96
FlorianVD <vandaele.florian@hotmail.com>
2020-06-09 20:58:07 +0200

    added extra methods to the userrepo

--- backend/api/MirrorOfErised/MirrorOfErised.models/Repos/IUserRepo.cs
+++ backend/api/MirrorOfErised/MirrorOfErised.models/Repos/IUserRepo.cs
@@ -6,5 +6,7 @@ namespace MirrorOfErised.models.Repos
     public interface IUserRepo
     {
         Task<List<User>> GetAllUsers();
+        Task<User> GetUserById(string id);
+        Task<User> Update(User user);
     }
 }
--- backend/api/MirrorOfErised/MirrorOfErised.models/Repos/UserRepo.cs
+++ backend/api/MirrorOfErised/MirrorOfErised.models/Repos/UserRepo.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MirrorOfErised.models.Data;
@@ -16,7 +17,21 @@ namespace MirrorOfErised.models.Repos
 
         public async Task<List<User>> GetAllUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.
+                OrderByDescending(u => u.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<User> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> Update(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
         }
     }
 }
