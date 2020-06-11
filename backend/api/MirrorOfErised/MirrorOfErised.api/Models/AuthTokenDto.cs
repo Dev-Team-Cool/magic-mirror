32026919f0300a400fdc96a4201a8e65a5f7c67d
FlorianVD <vandaele.florian@hotmail.com>
2020-06-11 21:46:31 +0200

    renamed and removed some properties

---
+++ backend/api/MirrorOfErised/MirrorOfErised.api/Models/AuthTokenDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+//using System.Text.Json.Serialization;
+
+namespace MirrorOfErised.api.Models
+{
+    public class AuthTokenDto
+    {
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
