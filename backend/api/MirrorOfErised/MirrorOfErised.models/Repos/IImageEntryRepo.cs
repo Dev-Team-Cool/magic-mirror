16c80c90a30ec2b970389e816bae0ec0f5d6a448
FlorianVD <vandaele.florian@hotmail.com>
2020-06-11 14:42:39 +0200

    count the amount of images for a certain user

--- backend/api/MirrorOfErised/MirrorOfErised.models/Repos/IImageEntryRepo.cs
+++ backend/api/MirrorOfErised/MirrorOfErised.models/Repos/IImageEntryRepo.cs
@@ -4,6 +4,7 @@ namespace MirrorOfErised.models.Repos
 {
     public interface IImageEntryRepo
     {
+        Task<int> CountImagesForUser(string userId);
         Task<ImageEntry> AddImage(ImageEntry image);
     }
 }
--- backend/api/MirrorOfErised/MirrorOfErised.models/Repos/ImageEntryRepo.cs
+++ backend/api/MirrorOfErised/MirrorOfErised.models/Repos/ImageEntryRepo.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using MirrorOfErised.models.Data;
 
 namespace MirrorOfErised.models.Repos
@@ -11,7 +13,12 @@ namespace MirrorOfErised.models.Repos
         {
             _context = context;
         }
-        
+
+        public async Task<int> CountImagesForUser(string userId)
+        {
+            return await _context.UserImages.Where(i => i.User.Id == userId ).CountAsync();
+        }
+
         public async Task<ImageEntry> AddImage(ImageEntry image)
         {
             await _context.UserImages.AddAsync(image);
