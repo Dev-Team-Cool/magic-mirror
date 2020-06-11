fa2d9ae3d42a1544398193fbbab17a602cea8fd8
FlorianVD <vandaele.florian@hotmail.com>
2020-06-11 21:52:07 +0200

    added new DTO's

---
+++ backend/api/MirrorOfErised/MirrorOfErised.api/Models/UserAddressDto.cs
@@ -0,0 +1,9 @@
+namespace MirrorOfErised.api.Models
+{
+    public class UserAddressDto
+    {
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
+    }
+}
---
+++ backend/api/MirrorOfErised/MirrorOfErised.api/Models/UserDto.cs
@@ -0,0 +1,14 @@
+using MirrorOfErised.models;
+
+namespace MirrorOfErised.api.Models
+{
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AuthTokenDto Tokens { get; set; }
+        public SettingsDto Settings { get; set; }
+        public UserEntryDto CommuteInfo { get; set; }
+    }
+}
