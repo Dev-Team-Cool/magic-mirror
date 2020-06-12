69b5e2b3bd0003b9fc2c33c76f479d61c5a669bf
FlorianVD <vandaele.florian@hotmail.com>
2020-06-12 15:31:50 +0200

    remove unnecessary modules

--- modules/config.js
+++ modules/config.js
@@ -35,68 +35,6 @@ var config = {
 			     // true, force serveronly mode, because you want to.. no UI on this device
 
 	modules: [
-		// {
-		// 	module: "alert",
-		// },
-		// {
-		// 	module: "updatenotification",
-		// 	position: "top_bar"
-		// },
-		// {
-		// 	module: "clock",
-		// 	position: "top_left"
-		// },
-		// {
-		// 	module: "calendar",
-		// 	header: "US Holidays",
-		// 	position: "top_left",
-		// 	config: {
-		// 		calendars: [
-		// 			{
-		// 				symbol: "calendar-check",
-		// 				url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
-		// 		]
-		// 	}
-		// },
-		// {
-		// 	module: "compliments",
-		// 	position: "lower_third"
-		// },
-		// {
-		// 	module: "currentweather",
-		// 	position: "top_right",
-		// 	config: {
-		// 		location: "New York",
-		// 		locationID: "", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-		// 		appid: "YOUR_OPENWEATHER_API_KEY"
-		// 	}
-		// },
-		// {
-		// 	module: "weatherforecast",
-		// 	position: "top_right",
-		// 	header: "Weather Forecast",
-		// 	config: {
-		// 		location: "New York",
-		// 		locationID: "5128581", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-		// 		appid: "YOUR_OPENWEATHER_API_KEY"
-		// 	}
-		// },
-		// {
-		// 	module: "newsfeed",
-		// 	position: "bottom_bar",
-		// 	config: {
-		// 		feeds: [
-		// 			{
-		// 				title: "New York Times",
-		// 				url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-		// 			}
-		// 		],
-		// 		showSourceTitle: true,
-		// 		showPublishDate: true,
-		// 		broadcastNewsFeeds: true,
-		// 		broadcastNewsUpdates: true
-		// 	}
-		// },
 		{
 			module: 'MMM-MyCommute',
 			position: 'top_right',
